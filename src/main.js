48307e0741687e51ac850668f35b3f4bd21a621c
chen <heart8858@gmail.com>
2020-11-09 21:47:28 +0800

    详情页参数和评论

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import moment from 'moment'
+
+// 定义全局时间戳过滤器
+Vue.filter('formatDate', function (value) {
+  return Moment(value).format('YYYY-MM-DD HH:mm:ss')
+})
 
 Vue.config.productionTip = false
 
--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,25 +1,49 @@
 <template>
   <div id="detail">
-    <detail-nav-bar />
-    <detail-swiper :top-images="topImages" />
+    <detail-nav-bar class="detail-nav" />
+    <scroll class="content" ref="scroll">
+      <detail-swiper :top-images="topImages" />
+      <detail-base-info :goods="goods" />
+      <detail-shop-info :shop="shop" />
+      <detail-goods-info :detail-info="detailInfo" @imageLoad="imageLoad" />
+      <detail-param-info :param-info="paramInfo" />
+      <detail-comment-info :comment-info="commentInfo" />
+    </scroll>
   </div>
 </template>
 
 <script>
 import DetailNavBar from "./childComps/DetailNavBar";
 import DetailSwiper from "./childComps/DetailSwiper";
-import { getDetail, Goods } from "network/detail";
+import DetailBaseInfo from "./childComps/DetailBaseInfo";
+import DetailShopInfo from "./childComps/DetailShopInfo";
+import DetailGoodsInfo from "./childComps/DetailGoodsInfo";
+import DetailParamInfo from "./childComps/DetailParamInfo";
+import DetailCommentInfo from "./childComps/DetailCommentInfo";
+
+import Scroll from "components/common/scroll/Scroll";
+import { getDetail, Goods, Shop, GoodsParam } from "network/detail";
 export default {
   name: "Detail",
   components: {
     DetailNavBar,
     DetailSwiper,
+    DetailBaseInfo,
+    DetailShopInfo,
+    DetailGoodsInfo,
+    DetailParamInfo,
+    DetailCommentInfo,
+    Scroll,
   },
   data() {
     return {
       iid: null,
       topImages: [],
-      goods: null,
+      goods: {},
+      shop: {},
+      detailInfo: {},
+      paramInfo: {},
+      commentInfo: {},
     };
   },
   created() {
@@ -38,10 +62,46 @@ export default {
         data.columns,
         data.shopInfo.services
       );
+
+      // 3.创建店铺信息
+      this.shop = new Shop(data.shopInfo);
+
+      // 4.保存商品的详情数据
+      this.detailInfo = data.detailInfo;
+
+      // 5.获取参数信息
+      this.paramInfo = new GoodsParam(
+        data.itemParams.info,
+        data.itemParams.rule
+      );
+
+      // 6.取出评论信息
+      if (data.rate.cRate !== 0) {
+        this.commentInfo = data.rate.list[0];
+      }
     });
+  },
+  methods: {
+    imageLoad() {
+      this.$refs.scroll.refresh();
+    },
   },
 };
 </script>
 
 <style scoped>
+#detail {
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+  height: 100vh;
+}
+.detail-nav {
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+}
+.content {
+  height: calc(100% - 44px);
+}
 </style>
