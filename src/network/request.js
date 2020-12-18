a8b93a66b4d74d07ad9907516430cd7ee0ec651b
chen <heart8858@gmail.com>
2020-12-18 17:30:32 +0800

    隐藏接口

--- src/network/request.js
+++ src/network/request.js
@@ -5,7 +5,7 @@ export default function axios(option) {
     return new Promise((resolve, reject) => {
         // 1.创建axios的实例
         const instance = originAxios.create({
-            baseURL: 'http://152.136.185.210:8000/api/w6',
+            baseURL: 'xxx',
             timeout: 5000
         });
 
